5ae519c8f6e2fb792e412d6c1b68b2a29cb73508
Jasneet Dua <jasneetdua96@gmail.com>
2023-02-01 00:14:54 +0530

    queried active session ids to render

--- popup-src/modules/c/popup/popup.js
+++ popup-src/modules/c/popup/popup.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
 
-export default class Popup extends LightningElement {}
+export default class Popup extends LightningElement {
+
+    shareItems = [];
+
+    connectedCallback(){
+        this.init();
+    }
+
+    async init(){
+        const cookies = await chrome.cookies.getAll({ name: 'sid' });
+        const sidCookies = cookies.filter((cookie) => {
+            return cookie.domain.includes('.my.salesforce.com');
+        });
+        this.shareItems = sidCookies.map(cookie => {
+            return {
+                domain: cookie.domain,
+                value: cookie.value
+            }
+        });
+    }
+}
