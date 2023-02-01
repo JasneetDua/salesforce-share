116b43e859761fe440b4a39dd10defff62bab73d
Jasneet Dua <jasneetdua96@gmail.com>
2023-02-01 22:37:52 +0530

    added styling and items to render properly

--- popup-src/modules/c/popup/popup.js
+++ popup-src/modules/c/popup/popup.js
@@ -1,23 +1,63 @@
 import { LightningElement } from 'lwc';
+import { CONSTANTS } from 'c/constants';
 
 export default class Popup extends LightningElement {
 
-    shareItems = [];
+    isLoading = true;
+    currentOrgId;
+    activeTabs = [];
+    activeOrgCookies = [];
+    currentOrgCookies = [];
+    otherActiveOrgCookies = [];
 
     connectedCallback(){
         this.init();
     }
 
+    get haveActiveSessions(){
+        return this.activeOrgCookies && this.activeOrgCookies.length;
+    }
+
+    get activePageUrl(){
+        return this.activeTabs && this.activeTabs.length && this.activeTabs[0].url;
+    }
+
+    get currentOrgCookie(){
+        return this.currentOrgCookies && this.currentOrgCookies.length && this.currentOrgCookies[0];
+    }
+
     async init(){
+        // get active page org id if its a salesforce page.
+        const params = { active: true, currentWindow: true };
+        const activeTabs = await chrome.tabs.query(params);
+        this.activeTabs = activeTabs;
+        if (activeTabs && activeTabs.length) {
+            try {
+                this.currentOrgId = await chrome.tabs.sendMessage(activeTabs[0].id, { action: CONSTANTS.GET_ORG_ID });
+            } 
+            catch (error) {
+                console.log('%cError ', 'background: red', 'content script not loaded');
+            }
+        }
+
+        // get all active sessions
         const cookies = await chrome.cookies.getAll({ name: 'sid' });
-        const sidCookies = cookies.filter((cookie) => {
+        const activeOrgCookies = cookies.filter((cookie) => {
             return cookie.domain.includes('.my.salesforce.com');
         });
-        this.shareItems = sidCookies.map(cookie => {
-            return {
-                domain: cookie.domain,
-                value: cookie.value
-            }
-        });
+        this.activeOrgCookies = activeOrgCookies;
+
+        if(this.currentOrgId){
+            // current org session cookie
+            this.currentOrgCookies = activeOrgCookies.filter((cookie) => {
+                return cookie.value.startsWith(this.currentOrgId);
+            });
+
+            // other orgs session cookie
+            this.otherActiveOrgCookies = activeOrgCookies.filter((cookie) => {
+                return !cookie.value.startsWith(this.currentOrgId);
+            });
+        }
+        this.isLoading = false;
     }
 }
--- popup-src/modules/c/shareItem/shareItem.js
+++ popup-src/modules/c/shareItem/shareItem.js
@@ -1,8 +1,29 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class ShareItem extends LightningElement {
 
-    @api urlLabel;
-    @api urlValue;
-    
+    @api label;
+    @api session;
+    @api retUrl;
+
+    @track labelToRender;
+    @track sublabelToRender;
+
+    connectedCallback() {
+        if (this.label) {
+            this.labelToRender = this.label;
+        }
+        else{
+            const myDomain = this.session.domain.replace('.my.salesforce.com', '');
+            const domainElements = myDomain.split('--');
+            if (domainElements.length > 1) {
+                this.labelToRender = domainElements[1];
+                this.sublabelToRender = domainElements[0];
+
+            }
+            else {
+                this.labelToRender = domainElements[0];
+            }
+        }
+    }
 }
