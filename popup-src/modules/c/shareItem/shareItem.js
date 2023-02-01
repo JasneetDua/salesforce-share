819db3d241b7817ad2de3d9eb452761523a9a4a5
Jasneet Dua <jasneetdua96@gmail.com>
2023-02-01 22:41:57 +0530

    added method to get session url

--- popup-src/modules/c/shareItem/shareItem.js
+++ popup-src/modules/c/shareItem/shareItem.js
@@ -3,27 +3,35 @@ import { LightningElement, api, track } from 'lwc';
 export default class ShareItem extends LightningElement {
 
     @api label;
+    @api subLabel;
     @api session;
     @api retUrl;
 
     @track labelToRender;
-    @track sublabelToRender;
+    @track subLabelToRender;
 
     connectedCallback() {
         if (this.label) {
             this.labelToRender = this.label;
+            this.subLabelToRender = this.subLabel;
         }
         else{
             const myDomain = this.session.domain.replace('.my.salesforce.com', '');
             const domainElements = myDomain.split('--');
             if (domainElements.length > 1) {
                 this.labelToRender = domainElements[1];
-                this.sublabelToRender = domainElements[0];
+                this.subLabelToRender = domainElements[0];
 
             }
             else {
                 this.labelToRender = domainElements[0];
             }
         }
+    }
+
+
+    getSessionUrl(session, retURL) {
+        const sessionUrl = `https://${session.domain}/secur/frontdoor.jsp?sid=${session.value}&retURL=${retURL}`;
+        return sessionUrl;
     }
 }
