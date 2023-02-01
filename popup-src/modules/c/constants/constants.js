32a17b54765fd3ecc92f8b9b692e1060e66afd08
Jasneet Dua <jasneetdua96@gmail.com>
2023-02-01 23:07:48 +0530

    code cleanup

--- popup-src/modules/c/constants/constants.js
+++ popup-src/modules/c/constants/constants.js
@@ -1,9 +1,5 @@
 const CONSTANTS = {
     GET_ORG_ID: 'GetOrgId',
-    COPY_TO_CLIP: 'CopyToClip',
-    SHOW_TOAST: 'ShowToast',
-    SHARE_HOME_PAGE: 'ShareHomePage',
-    SHARE_CURRENT_PAGE: 'ShareCurrentPage',
 };
 
 export {
